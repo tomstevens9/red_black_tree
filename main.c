56214730438e6c8d1b0f36de2e723317c26ebaea
Tom Stevens <tomjstevens9@gmail.com>
2021-04-05 19:33:34 +0100

    WIP: New insert code

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 #include <inttypes.h>
 #include <stdarg.h>
 #include <math.h>
+#include <assert.h>
 
 // TODO add LEFT_CHILD and RIGHT_CHILD defines?
 
-#define LEFT 0
-#define RIGHT 1
 #define RB_TREE_DEBUG_LOG 1
 
 enum rb_color { RED, BLACK };
+enum rb_dir { LEFT=0, RIGHT=1 };
 
 struct RBNode {
     int value;
@@ -64,7 +64,7 @@ void rb_tree_swap_colors(struct RBNode *n1, struct RBNode *n2) {
 
 
 // TODO REPLACE AS MACRO?
-uint8_t rb_tree_orientation(struct RBNode *node) {
+enum rb_dir rb_tree_dir(struct RBNode *node) {
     // ASSUMES NODE IS NOT ROOT
     return (node->value < node->parent->value) ? LEFT : RIGHT;
 }
@@ -73,7 +73,7 @@ uint8_t rb_tree_orientation(struct RBNode *node) {
 // TODO REPLACE AS MACRO?
 struct RBNode *rb_tree_sibling(struct RBNode *node) {
     // ASSUMES NODE IS NOT ROOT
-    return (rb_tree_orientation(node) == LEFT) ? node->parent->children[1] : node->parent->children[0];
+    return (rb_tree_dir(node) == LEFT) ? node->parent->children[1] : node->parent->children[0];
 }
 
 // TODO REPLACE AS MACRO?
@@ -95,7 +95,7 @@ void rb_tree_left_rotate(struct RBTree *tree, struct RBNode *node) {
     if (child->parent == NULL) {
         tree->root = child;
     } else {
-        if (rb_tree_orientation(node) == LEFT) {
+        if (rb_tree_dir(node) == LEFT) {
             child->parent->children[0] = child;
         } else {
             child->parent->children[1] = child;
@@ -118,7 +118,7 @@ void rb_tree_right_rotate(struct RBTree *tree, struct RBNode *node) {
     if (child->parent == NULL) {
         tree->root = child;
     } else {
-        if (rb_tree_orientation(node) == LEFT) {
+        if (rb_tree_dir(node) == LEFT) {
             child->parent->children[0] = child;
         } else {
             child->parent->children[1] = child;
@@ -145,16 +145,16 @@ void rb_tree_fixup(struct RBTree *tree, struct RBNode *node) {
         grandparent = rb_tree_grandparent(node);
         // uncle is explicitly black or implicitly black due to being a NIL node
         if (uncle == NULL || uncle->color == BLACK) {
-            if (rb_tree_orientation(node) == RIGHT && rb_tree_orientation(parent) == LEFT) {
+            if (rb_tree_dir(node) == RIGHT && rb_tree_dir(parent) == LEFT) {
                 rb_tree_left_rotate(tree, parent);
                 node = node->children[0];
-            } else if (rb_tree_orientation(node) == LEFT && rb_tree_orientation(parent) == RIGHT) {
+            } else if (rb_tree_dir(node) == LEFT && rb_tree_dir(parent) == RIGHT) {
                 rb_tree_right_rotate(tree, node->parent);
                 node = node->children[1];
-            } else if (rb_tree_orientation(node) == LEFT && rb_tree_orientation(parent) == LEFT) {
+            } else if (rb_tree_dir(node) == LEFT && rb_tree_dir(parent) == LEFT) {
                 rb_tree_right_rotate(tree, grandparent);
                 rb_tree_swap_colors(node->parent, node->parent->children[1]);
-            } else if (rb_tree_orientation(node) == RIGHT && rb_tree_orientation(parent) == RIGHT) {
+            } else if (rb_tree_dir(node) == RIGHT && rb_tree_dir(parent) == RIGHT) {
                 rb_tree_left_rotate(tree, grandparent);
                 rb_tree_swap_colors(node->parent, node->parent->children[0]);
             }
@@ -193,6 +193,106 @@ void rb_tree_add(struct RBTree *tree, int n) {
         new_node->color = RED;
         rb_tree_fixup(tree, new_node);
     }
+}
+
+struct RBNode * rb_tree_rotate(struct RBTree *tree, struct RBNode *p, enum rb_dir dir) {
+    struct RBNode *g, *s, *c;
+    printf("ROTATE START - %s\n", (dir == LEFT) ? "LEFT" : "RIGHT");
+    printf("%d\n", p->value);
+    g = p->parent;
+    s = p->children[1-dir];
+    assert(s != NULL);
+    c = s->children[dir];
+    p->children[1-dir] = c;
+    if (c != NULL) c->parent = p;
+    s->children[dir] = p;
+    if (g != NULL) {
+        g->children[(p == g->children[RIGHT]) ? RIGHT : LEFT] = s;
+    } else {
+        tree->root = s;
+    }
+    printf("ROTATE FINISH\n");
+    return s;
+}
+
+void rb_tree_insert_node(struct RBTree *tree, struct RBNode *p, struct RBNode* n, enum rb_dir dir) {
+    struct RBNode *g, *u;
+    // add the node into the tree
+    n->color = RED;
+    n->children[LEFT] = NULL;
+    n->children[RIGHT] = NULL;
+    n->parent = p;
+    if (p == NULL) {
+        n->color = BLACK;
+        tree->root = n;
+        return;
+    }
+    p->children[dir] = n;
+    do {
+        // nothing more to resolve
+        if (p->color == BLACK) return;
+        // root is red - change to black
+        if ((g = p->parent) == NULL) {
+            p->color = BLACK;
+            return;
+        }
+        dir = rb_tree_dir(p);
+        u = g->children[1-dir];
+        if (u == NULL || u->color == BLACK) {
+            if (n == p->children[1-dir]) {
+                rb_tree_rotate(tree, p, dir);
+                n = p;
+                p = g->children[dir];
+            }
+            rb_tree_rotate(tree, g, 1-dir);
+            p->color = BLACK;
+            g->color = RED;
+            return;
+        }
+        p->color = BLACK;
+        u->color = BLACK;
+        g->color = RED;
+        n = g;
+    } while((p = n->parent) != NULL);
+}
+
+void rb_tree_insert(struct RBTree *tree, int val) {
+    struct RBNode *n, *p;
+    enum rb_dir dir;
+    printf("INSERT - %d\n", val);
+    n = calloc(1, sizeof(struct RBNode));
+    n->value = val;
+    p = tree->root;
+    // tree is empty
+    if (p == NULL) {
+        rb_tree_insert_node(tree, p, n, 0);  // dir argument is irrelevant here
+        return;
+    }
+    while (1) {
+        if (val == p->value) return;
+        if (val < p->value) {
+            if (p->children[LEFT] == NULL) {
+                rb_tree_insert_node(tree, p, n, LEFT);
+                return;
+            }
+            p = p->children[LEFT];
+        } else {
+            if (p->children[RIGHT] == NULL) {
+                rb_tree_insert_node(tree, p, n, RIGHT);
+                return;
+            }
+            p = p->children[RIGHT];
+        }
+    }
+    return;
+    // find where to insert node in the tree
+    dir = (val < p->value) ? LEFT : RIGHT;
+    while (p->children[dir] != NULL) {
+        if (val == p->value) return;  // do nothing if already in tree
+        p = p->children[dir];
+        dir = (val < p->value) ? LEFT : RIGHT;
+    }
+    rb_tree_insert_node(tree, p, n, dir);
 }
 
 
@@ -249,29 +349,29 @@ void rb_tree_fix_double_black(struct RBTree *tree, struct RBNode *node,  struct 
             rb_tree_fix_double_black(tree, parent, parent->parent);
         }
     } else if (sibling->color == BLACK && rb_tree_has_red_child(sibling)) {
-        if (rb_tree_orientation(sibling) == RIGHT && rb_tree_is_red(sibling->children[LEFT])) {
+        if (rb_tree_dir(sibling) == RIGHT && rb_tree_is_red(sibling->children[LEFT])) {
             sibling->children[LEFT]->color = BLACK;
             rb_tree_right_rotate(tree, sibling);
             rb_tree_left_rotate(tree, parent);
-        } else if (rb_tree_orientation(sibling) == RIGHT && rb_tree_is_red(sibling->children[RIGHT])) {
+        } else if (rb_tree_dir(sibling) == RIGHT && rb_tree_is_red(sibling->children[RIGHT])) {
             parent->color = RED;
             rb_tree_left_rotate(tree, parent);
-        } else if (rb_tree_orientation(sibling) == LEFT && rb_tree_is_red(sibling->children[RIGHT])) {
+        } else if (rb_tree_dir(sibling) == LEFT && rb_tree_is_red(sibling->children[RIGHT])) {
             sibling->children[RIGHT]->color = BLACK;
             rb_tree_left_rotate(tree, sibling);
             rb_tree_right_rotate(tree, parent);
-        } else if (rb_tree_orientation(sibling) == LEFT && rb_tree_is_red(sibling->children[LEFT])) {
+        } else if (rb_tree_dir(sibling) == LEFT && rb_tree_is_red(sibling->children[LEFT])) {
             parent->color = RED;
             rb_tree_right_rotate(tree, parent);
             
         }
    } else if (sibling->color == RED) {
         printf("Here Z\n");
-        if (rb_tree_orientation(sibling) == RIGHT) {
+        if (rb_tree_dir(sibling) == RIGHT) {
             rb_tree_left_rotate(tree, parent);
             rb_tree_swap_colors(parent, parent->parent);
             rb_tree_fix_double_black(tree, node, parent);
-        } else if (rb_tree_orientation(sibling) == LEFT) {
+        } else if (rb_tree_dir(sibling) == LEFT) {
             rb_tree_left_rotate(tree, parent);
             rb_tree_swap_colors(parent, parent->parent);
             rb_tree_fix_double_black(tree, node, parent);
@@ -293,7 +393,7 @@ void rb_tree_remove(struct RBTree *tree, int n) {
         if (node->parent == NULL) {
             tree->root = NULL;
         } else {
-            node->parent->children[rb_tree_orientation(node)] = NULL;
+            node->parent->children[rb_tree_dir(node)] = NULL;
             if (node->color == BLACK) {
                 rb_tree_fix_double_black(tree, NULL, node->parent);
             }
@@ -303,7 +403,7 @@ void rb_tree_remove(struct RBTree *tree, int n) {
     if (child_count == 1) {
         struct RBNode *child = (node->children[LEFT] == NULL) ? node->children[RIGHT] : node->children[LEFT];
         node->value = child->value;
-        node->children[rb_tree_orientation(child)] = NULL; // TODO can probably be done more efficiently
+        node->children[rb_tree_dir(child)] = NULL; // TODO can probably be done more efficiently
         if (node->color == RED || child->color == RED) {
             node->color = BLACK;
         } else {
@@ -324,8 +424,10 @@ int main() {
     int shuffled_nums[] = {274, 470, 218, 630, 233, 622, 951, 726, 783, 520, 836, 652, 414, 16, 667, 769, 148, 807, 603, 746, 565, 998, 762, 134, 246, 666, 289, 299, 416, 669, 544, 304, 688, 204, 41, 488, 9, 764, 733, 127, 617, 820, 951, 655, 706, 615, 42, 898, 215, 940, 856, 736, 824, 447, 207, 834, 18, 954, 318, 61, 406, 849, 130, 295, 260, 826, 943, 541, 409, 451, 528, 926, 162, 102, 18, 203, 260, 590, 252, 262, 51, 714, 670, 998, 981, 248, 804, 207, 178, 607, 578, 435, 288, 88, 635, 705, 519, 486, 415, 81};
     struct RBTree tree = rb_tree_init();
     for (int i = 0; i < 100; i++) {
-        rb_tree_add(&tree, nums[i]);
+        rb_tree_insert(&tree, nums[i]);
+        debug_traverse(tree.root, 0, 0, 0);
     }
+    exit(1);
     for (int i = 0; i < 100; i++) {
         if (i == 33) {
             printf("----------------------------------\n");
